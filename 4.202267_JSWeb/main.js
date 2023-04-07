e7258c93861d9c0b33d2b15a88e8840f20143b3c
taohansens <taohansens@gmail.com>
2023-04-07 18:01:19 -0300

    For. AluraPhone.js

--- 4.202267_JSWeb/main.js
+++ 4.202267_JSWeb/main.js
@@ -5,15 +5,13 @@ function tocaSom(idElementoAudio) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
-while (contador < listaDeTeclas.length) {
+for (let i = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
 
     const idAudio = `#som_${instrumento}`;
 
-    tecla.onclick = function (){
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
-    contador += 1;
 }
