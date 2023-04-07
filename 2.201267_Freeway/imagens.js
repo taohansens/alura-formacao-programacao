8436eeaad59b5fefa23d9852313bc2d455f3bdda
taohansens <taohansens@gmail.com>
2023-04-06 21:08:07 -0300

    new Cars, logic to show again

--- 2.201267_Freeway/imagens.js
+++ 2.201267_Freeway/imagens.js
@@ -1,9 +1,13 @@
 let imagemDaEstrada;
 let imagemDoAtor;
 let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
 
 function preload(){
     imagemDaEstrada = loadImage("assets/images/estrada.png");
     imagemDoAtor = loadImage("assets/images/ator-1.png");
     imagemCarro = loadImage("assets/images/carro-1.png");
+    imagemCarro2 = loadImage("assets/images/carro-2.png");
+    imagemCarro3 = loadImage("assets/images/carro-3.png");
 }
