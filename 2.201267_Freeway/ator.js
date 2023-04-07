61d0d5003f715935129f1bca678b313d1a6ce189
taohansens <taohansens@gmail.com>
2023-04-06 21:33:03 -0300

    score negative, restrictions

--- 2.201267_Freeway/ator.js
+++ 2.201267_Freeway/ator.js
@@ -7,21 +7,25 @@ function mostraAtor(){
     image(imagemDoAtor, xAtor, yAtor, 30, 30);
 }
 
-function movimentaAtor(){
-    if (keyIsDown(UP_ARROW)){
+function movimentaAtor() {
+    if (keyIsDown(UP_ARROW)) {
         yAtor -= 3;
     }
-    if (keyIsDown(DOWN_ARROW)){
-        yAtor += 3;
+    if (keyIsDown(DOWN_ARROW)) {
+        if (podeSeMover()) {
+            yAtor += 3;
+        }
     }
 }
 
 function verificaColisao(){
-    //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
     for (let i = 0; i < imagemCarros.length; i++){
         colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
         if (colisao){
             voltaAtorParaPosicaoInicial();
+            if (meusPontos > 0){
+                meusPontos -=1;
+            }
         }
     }
 }
@@ -40,4 +44,7 @@ function marcaPonto(){
         meusPontos += 1;
         voltaAtorParaPosicaoInicial();
     }
+}
+function podeSeMover(){
+    return yAtor < 366;
 }
