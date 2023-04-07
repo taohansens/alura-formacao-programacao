6dc7adc74ebfe457469e31d8dd22ea9302873bb9
taohansens <taohansens@gmail.com>
2023-04-07 17:47:01 -0300

    list and idAudio dynamic.

--- 4.202267_JSWeb/main.js
+++ 4.202267_JSWeb/main.js
@@ -1,5 +1,19 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play()
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+    contador += 1;
+}
