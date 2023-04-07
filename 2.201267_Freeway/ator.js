344a990afd9c8312a2f9b7100360124f7a55ccd8
taohansens <taohansens@gmail.com>
2023-04-06 21:28:18 -0300

    scoreboard, and logic to score

--- 2.201267_Freeway/ator.js
+++ 2.201267_Freeway/ator.js
@@ -1,6 +1,7 @@
 let xAtor = 100;
 let yAtor = 366;
 let colisao = false;
+let meusPontos = 0;
 
 function mostraAtor(){
     image(imagemDoAtor, xAtor, yAtor, 30, 30);
@@ -14,9 +15,6 @@ function movimentaAtor(){
         yAtor += 3;
     }
 }
-function voltaAtorParaPosicaoInicial(){
-    yAtor = 366;
-}
 
 function verificaColisao(){
     //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
@@ -27,7 +25,19 @@ function verificaColisao(){
         }
     }
 }
-
-function colidiu(){
+function voltaAtorParaPosicaoInicial(){
     yAtor = 366;
+}
+function incluiPontos(){
+    textAlign(CENTER);
+    textSize(25);
+    fill(color(255, 240, 60))
+    text(meusPontos, width / 5, 27);
+}
+
+function marcaPonto(){
+    if (yAtor < 15){
+        meusPontos += 1;
+        voltaAtorParaPosicaoInicial();
+    }
 }
--- 2.201267_Freeway/freeway.js
+++ 2.201267_Freeway/freeway.js
@@ -10,4 +10,6 @@ function draw() {
     movimentaAtor();
     voltaPosicaoInicialDoCarro();
     verificaColisao();
+    incluiPontos();
+    marcaPonto();
 }
