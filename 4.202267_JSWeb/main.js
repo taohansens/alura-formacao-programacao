cdce594a43f7ab462cb4c5abff440819e47b2f52
taohansens <taohansens@gmail.com>
2023-04-07 18:18:40 -0300

    Buttons keyboard on Js. Finished Course.

--- 4.202267_JSWeb/main.js
+++ 4.202267_JSWeb/main.js
@@ -1,5 +1,10 @@
 function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play()
+    const elemento = document.querySelector(idElementoAudio);
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado.');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -13,5 +18,15 @@ for (let i = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 }
