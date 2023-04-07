a94eb81508e2a070ce6e6b1d662282840bee96ca
taohansens <taohansens@gmail.com>
2023-04-06 21:03:10 -0300

    responsibilities separated

---
+++ 2.201267_Freeway/imagens.js
@@ -0,0 +1,9 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+
+function preload(){
+    imagemDaEstrada = loadImage("assets/images/estrada.png");
+    imagemDoAtor = loadImage("assets/images/ator-1.png");
+    imagemCarro = loadImage("assets/images/carro-1.png");
+}
