d6eb4951684e0867aba3a2bfdb919e3217136089
taohansens <taohansens@gmail.com>
2023-04-16 20:16:07 -0300

    Finish Course

--- 5.202108_TestesTDDJavaJunit/src/test/java/src/service/BonusServiceTest.java
+++ 5.202108_TestesTDDJavaJunit/src/test/java/src/service/BonusServiceTest.java
@@ -1,9 +1,9 @@
 package src.service;
 
-
 import br.com.alura.tdd.modelo.Funcionario;
 import br.com.alura.tdd.service.BonusService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import src.Factory;
 
@@ -11,22 +11,23 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class BonusServiceTest {
+    private BonusService service;
+    @BeforeEach
+    public void setUp() {
+        this.service = new BonusService();
+    }
 
     @Test
     void bonusShouldThrowIllegalArgumentExceptionEarnMoreThan1000() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            BonusService service = new BonusService();
             Funcionario funcionario = Factory.createFuncionario(25000.0);
             BigDecimal expected = new BigDecimal(0).setScale(2, RoundingMode.HALF_UP);;
-
             BigDecimal bonus = service.calcularBonus(funcionario);
         });
     }
 
     @Test
     void bonusShouldBe10perCentSalaryWhenEarnExactly1000() {
-        BonusService service = new BonusService();
-
         double salary = 1000.0;
         BigDecimal expected = new BigDecimal(salary * 0.1).setScale(2, RoundingMode.HALF_UP);;
 
@@ -39,8 +40,6 @@ public class BonusServiceTest {
 
     @Test
     void bonusShouldBe10perCentSalaryWhenEarnLessThan1000() {
-        BonusService service = new BonusService();
-
         double salary = 2000.0;
         BigDecimal expected = new BigDecimal(salary * 0.1).setScale(2, RoundingMode.HALF_UP);;
 
